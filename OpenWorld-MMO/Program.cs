721c2b49b3f59027ba9bd10af296989d46834274
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2022-09-12 15:30:28 +0200

    Setting up scene

--- OpenWorld-MMO/Program.cs
+++ OpenWorld-MMO/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace OpenWorld_MMO
+{
+    public static class Program{ 
+        
+        static string _phrase = "";
+        
+        public static void Main()
+        {
+            var ipEndPoint = new IPEndPoint(IPAddress.Any, 44444);
+            var udpClient = new UdpClient(ipEndPoint);
+            Console.WriteLine("Server has started");
+
+            while (true)
+            {
+                var sender = new IPEndPoint(IPAddress.Any, 0);
+                var data = udpClient.Receive(ref sender);
+                Console.WriteLine(udpClient + " connected");
+                
+                var word = FilterWord(data);
+                data = ValidateWord(word);
+                
+                udpClient.Send(data, data.Length, sender); 
+            }
+        } 
+        
+        private static string FilterWord(byte[] data)
+        {
+            var temp = Encoding.ASCII.GetString(data, 0, data.Length);
+            Console.WriteLine("Word received: " + temp);
+            char whiteSpace = ' ';
+            char backSlash = '\n';
+            char questionMark = '?';
+            string result = "";
+            
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if(temp[i].CompareTo(whiteSpace) == 0 ||
+                   temp[i].CompareTo(backSlash) == 0 ||
+                   temp[i].CompareTo(questionMark) == 0) break;
+                
+                result += temp[i];
+            }
+            
+            return result + " ";
+        } 
+        private static byte[] ValidateWord(string word)
+        {
+            if (word.Length > 21)
+                return PreparePackageToSend("Invalid word\n"); 
+            
+            return PreparePackageToSend(_phrase += word);
+        } 
+        
+        private static byte[] PreparePackageToSend(string quote) => Encoding.ASCII.GetBytes(quote + '\n');
+
+    }
+}
