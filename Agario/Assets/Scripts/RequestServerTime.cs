5a751c1e6d5bb1ad6dcfa1438a1b974dffd9fcae
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2022-09-12 13:55:43 +0200

    New try

--- Agario/Assets/Scripts/RequestServerTime.cs
+++ Agario/Assets/Scripts/RequestServerTime.cs
@@ -12,23 +12,26 @@ using UnityEngine.UI;
 
 public class RequestServerTime : MonoBehaviour
 {
-    public Text timeText;
+    [SerializeField] private Text timeText;
 
     public void SendRequest()
    {
       var tcpClient = new TcpClient("127.0.0.1", 14411);
 
       var stream = tcpClient.GetStream();
-      byte[] bytes = new byte [tcpClient.ReceiveBufferSize];
+      var bytes = new byte [tcpClient.ReceiveBufferSize];
       stream.Read(bytes, 0, bytes.Length);
 
-      var text = Encoding.ASCII.GetString(bytes);
-      timeText.text = text;
-
+      string message = Encoding.ASCII.GetString(bytes);
+      
+    UpdateText(message);
 
    }
 
-  
+    private void UpdateText(string message)
+    {
+        timeText.text = message;
+    }
 
 
 }
