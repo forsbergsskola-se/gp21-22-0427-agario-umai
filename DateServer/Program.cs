1978449f7fa7cd38f273071a40d551abc81cd3f3
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2022-10-21 18:35:47 +0200

    Trying out solutions

--- DateServer/Program.cs
+++ DateServer/Program.cs
@@ -1,14 +1,30 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
 //Creating a listener, setting IP server address
-var tcpListener = new TcpListener(IPAddress.Loopback, 14411);
 
-//Start server/listener 
-tcpListener.Start();
-Console.WriteLine("Start Listener");
+List<int> ports = new(){14411,3002,3003,5521,3004};
+TcpListener tcpListener = null;
+
+for (int i = 0; i < ports.Count; i++)
+{
+    try
+    {
+        tcpListener = new TcpListener(IPAddress.Loopback, ports[i]);
+        tcpListener.Start();
+        Console.WriteLine("Start Listener on port " + ports[i]);
+        break;
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Port " + ports[i] + " is already running" );
+       
+    }
+}
 
 //Keep server running
 while (true)
